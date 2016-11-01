54a9d0a01d05e5f1548b2b7d3c43dc2ed9a1ae5b
sherrellbc <sherrellbc@gmail.com>
2016-11-01 19:28:22 -0400

    Preliminary at24cx eeprom read/write/erase support

--- src/at24cx_driver.cpp
+++ src/at24cx_driver.cpp
@@ -16,7 +16,7 @@ static uint16_t g_current_addr = 0x00;
  *
  * @param addr  : The address to set 
  */
-static int at24cx_set_addr_register(uint16_t addr){
+static int at24cx_set_read_addr(uint16_t addr){
     Wire.beginTransmission(AT24CX_I2C_ADDR);
     Wire.write( (uint8_t) ((addr>>8) & 0x00FF) );
     Wire.write( (uint8_t) (addr & 0x00FF) );
@@ -32,6 +32,39 @@ static int at24cx_set_addr_register(uint16_t addr){
 
 
 
+/*
+ * The at24cx does not respond to bus queries while busy. As 
+ * such, we can discern device readiness by checking for a 
+ * bus address confirmation
+ *
+ * @return  :   1 if the at24cx is ready, otherwise 0
+ */
+static int at24cx_ready(void){
+    Wire.beginTransmission(AT24CX_I2C_ADDR);
+    Wire.endTransmission();
+    
+    /* Device is not ready */
+    if(0 != Wire.getError())
+        return 0;
+    
+    return 1; 
+}
+
+
+
+/*
+ * Convert the given page number to its respective physical address
+ *
+ * @param page_num  :   The page number to convert
+ *
+ * @return  : The physical address corresponding to page_num
+ */
+static uint16_t at24cx_page_to_addr(uint16_t page_num){
+    return (page_num*AT24CX_PAGE_SIZE); 
+}
+
+
+
 int at24cx_init(){
     /* Ensure the device is on the bus */
     Wire.beginTransmission(AT24CX_I2C_ADDR);
@@ -42,7 +75,7 @@ int at24cx_init(){
     }
     
     /* Set the internal register to 0x00 by default */
-    if(0 != at24cx_set_addr_register(0x0000))
+    if(0 != at24cx_set_read_addr(0x0000))
         return -1; 
     
     return 0; 
@@ -50,43 +83,21 @@ int at24cx_init(){
 
 
 
-int at24cx_write(uint16_t addr, uint8_t *buf, int count){
+int at24cx_read(uint16_t addr, uint8_t *buf, int len){
     int i;
 
-    //if(0 != at24cx_set_addr_register(addr))
-    //    return -1; 
+    /* Wait for the device to be ready for the requsted read operation */
+    while(!at24cx_ready())
+        ;
 
-    /* Loop and read "count" bytes of data */
-    Wire.beginTransmission(AT24CX_I2C_ADDR);
-
-    Wire.send( (uint8_t) ((addr>>8) & 0x00FF) );
-    Wire.send( (uint8_t) (addr & 0x00FF) );
-
-    Wire.write(buf, count); 
-    Wire.endTransmission();
-
-    /* Check for error */
-    if(0 != Wire.getError()){
-        g_err_code = AT24CX_NO_RESPONSE; 
-        return -1;  
-    }
-
-    return 0; 
-} 
-
-
-
-int at24cx_read(uint16_t addr, uint8_t *buf, int count){
-    int i;
-
-    if(0 != at24cx_set_addr_register(addr))
+    if(0 != at24cx_set_read_addr(addr))
         return -1; 
 
-    /* Loop and read "count" bytes of data */
-    if(count != (int) Wire.requestFrom(AT24CX_I2C_ADDR, count))
+    /* Loop and read "len" bytes of data */
+    if(len != (int) Wire.requestFrom(AT24CX_I2C_ADDR, len))
         return -1; 
 
-    for(i=0; i<count; i++)
+    for(i=0; i<len; i++)
         buf[i] = Wire.read();
 
     /* Check for error */
@@ -95,18 +106,89 @@ int at24cx_read(uint16_t addr, uint8_t *buf, int count){
         return -1;  
     }
 
+    g_current_addr = addr + len; 
     return 0; 
 }
 
 
 
-int at24cx_page_erase(){
-    return 0;
+static int at24cx_page_write(uint16_t addr, uint8_t *buf){
+    /* Wait for the device to be ready for the requsted write operation */
+    while(!at24cx_ready())
+        ;
+                                                                           
+    /* Set the destination address and write "len" bytes */
+    Wire.beginTransmission(AT24CX_I2C_ADDR);
+    Wire.send( (uint8_t) ((addr>>8) & 0x00FF) );
+    Wire.send( (uint8_t) (addr      & 0x00FF) );
+    Wire.write(buf, AT24CX_PAGE_SIZE); 
+    Wire.endTransmission();
+                                                                           
+    /* Check for error */
+    if(0 != Wire.getError()){
+        g_err_code = AT24CX_NO_RESPONSE; 
+        return -1;  
+    }
+                                                                           
+    g_current_addr = addr + AT24CX_PAGE_SIZE;
+    return 0; 
+}
+
+
+
+/* 
+ * TODO: Implementation is broken for writing multiple pages 
+ *   -- Internal addr counter of the at24cx rolls over to the same page 
+*/
+int at24cx_write(uint16_t addr, uint8_t *buf, int len){
+    /* Wait for the device to be ready for the requsted write operation */
+    while(!at24cx_ready())
+        ;
+
+    /* Set the destination address and write "len" bytes */
+    Wire.beginTransmission(AT24CX_I2C_ADDR);
+    Wire.send( (uint8_t) ((addr>>8) & 0x00FF) );
+    Wire.send( (uint8_t) (addr      & 0x00FF) );
+    
+    if(NULL == buf){
+        while(len--)
+            Wire.write((uint8_t)0);
+    }else
+        Wire.write(buf, len); 
+    Wire.endTransmission();
+
+    /* Check for error */
+    if(0 != Wire.getError()){
+        g_err_code = AT24CX_NO_RESPONSE; 
+        return -1;  
+    }
+
+    g_current_addr = addr + len; 
+    return 0; 
+} 
+
+
+
+int at24cx_erase(uint16_t addr, int len){
+    return at24cx_write(addr, NULL, len); 
+}  
+
+
+
+int at24cx_page_erase(uint16_t page_num){
+    return at24cx_erase(at24cx_page_to_addr(page_num), AT24CX_PAGE_SIZE);
 }  
 
 
 
 int at24cx_full_erase(){
+    int i;
+
+    for(i=0; i<AT24CX_NUM_PAGES; i++){
+        if(0 != at24cx_erase(at24cx_page_to_addr(i), AT24CX_PAGE_SIZE))
+            return -1;
+    }    
+    
     return 0; 
 }
 
--- src/include/at24cx.h
+++ src/include/at24cx.h
@@ -8,12 +8,26 @@ extern "C" {
 #endif
 
 
-#define PAGE_SIZE       64
+#define AT24CX_PAGE_SIZE    64
+#define AT24CX_NUM_PAGES    512
+    
 
 /* i2c bus address */
 #define DEV_A0              0
 #define DEV_A1              1
 #define AT24CX_I2C_ADDR     0x50 | DEV_A1<<1 | DEV_A0
+
+
+/*
+ * TODO: All writes/reads are limited by the i2c_t3 I2C_TX_BUFFER_LENGTH buffer limit
+ *  - Ensure this implementation properly segments larger requests
+ *
+ * TODO: All write implementations do not take into account the current address and 
+ *  the requested number of bytes. Segmenting the writes across pages will require
+ *  some attention.  
+ */
+ 
+
 
 
 enum at24cx_err {
@@ -38,11 +52,11 @@ int at24cx_init();
  *
  * @param addr  : Address to read from
  * @param buf   : Preallocated destination buffer to place data read from the device
- * @param count : Number of bytes to read
+ * @param len   : Number of bytes to read
  *
  * @return  : -1 on error
  */
-int at24cx_read(uint16_t addr, uint8_t *buf, int count); 
+int at24cx_read(uint16_t addr, uint8_t *buf, int len); 
 
 
 
@@ -51,11 +65,43 @@ int at24cx_read(uint16_t addr, uint8_t *buf, int count);
  * 
  * @param addr  : Address to write
  * @param buf   : Buffer containing data to write
- * @param count : Number of bytes to write
+ * @param len   : Number of bytes to write
  *
  * @return  : -1 on error
  */ 
-int at24cx_write(uint16_t addr, uint8_t *buf, int count); 
+int at24cx_write(uint16_t addr, uint8_t *buf, int len); 
+
+
+
+/*
+ * Erase (set to 0x00) memory
+ *
+ * @param addr  : Address to begin erasing
+ * @param len   : Number of bytes to erase
+ *
+ * @error   : -1 on error
+ */
+int at24cx_erase(uint16_t addr, int len);
+
+
+
+/*
+ * Erase a specified page 
+ *
+ * @param page_num  :   The page to erase
+ *
+ * @return  : -1 on failure
+ */
+int at24cx_page_erase(uint16_t page_num);
+
+
+
+/*
+ * Perform a full erase
+ *
+ * @return  : -1 on error
+ */
+int at24cx_full_erase(void);
 
 
 
@@ -65,6 +111,8 @@ int at24cx_write(uint16_t addr, uint8_t *buf, int count);
  * @return : The last error mode
  */
 enum at24cx_err at24cx_read_error(void);
+
+
 
 #ifdef __cplusplus
 }
